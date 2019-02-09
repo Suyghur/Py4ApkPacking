a5580143cb7091ce0d523a97c35bf62ced2f695a
maijinpei <maijinpei@3k.com>
2019-02-10 04:02:08 +0800

    Delete tl_SpecialHandleToolKit.py
    移除SpecaalHandleToolKit

--- Package_Plugin/Tools/tl_SpecialHandleToolKit.py
+++
@@ -1,9 +0,0 @@
-# _*_coding:utf-8_*_
-# Created by #Suyghur, on 2019-02-09.
-# Copyright (c) 2019 3KWan.
-# Description : special handing of games
-
-
-class SpecialHandleToolKit:
-    def __init__(self):
-        pass
